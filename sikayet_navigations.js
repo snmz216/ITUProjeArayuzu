4d894afb0dc2c4267633846407536d6ba3b3b233
unlut <turkerunlu@hotmail.com>
2018-07-10 11:14:07 +0300

    Navigation javascript

---
+++ sikayet_navigations.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    //  sikayet button
+    $("#navigationButton0").on("click", function() {
+        window.location.href = "sema_3_1_1_1.html"
+    });
+
+    //  konum button
+    $("#navigationButton1").on("click", function() {
+        window.location.href = "sema_3_1_2_1.html"
+    });
+
+    //  kaynak button
+    $("#navigationButton2").on("click", function() {
+        window.location.href = "sema_3_1_3_1.html"
+    });
+
+    //  zaman button
+    $("#navigationButton3").on("click", function() {
+        window.location.href = "sema_3_1_4_0.html"
+    });
+
+    //  ek bilgi button
+    $("#navigationButton4").on("click", function() {
+        window.location.href = "sema_3_1_5_0.html"
+    });
+})
