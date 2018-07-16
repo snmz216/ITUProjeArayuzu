e55dd0ae4538701e845726b368ab068507b1b25d
unlut <turkerunlu@hotmail.com>
2018-07-16 22:31:25 +0300

    next button functionality

--- sikayet_navigations.js
+++ sikayet_navigations.js
@@ -28,4 +28,43 @@ $(document).ready(function() {
     $("#cancelButton").on("click", function() {
         window.location.href = "sema_3.html";
     })
+
+    //  next button
+    $("#nextButton").on("click", function() {
+        //  check if hidden page div exists
+        if ( $("#pageDataDiv").length )
+        {
+            console.log("hidden page data found");
+
+
+            //  check if data has data-add-sikayet-page-index attribute
+            var attr = $("#pageDataDiv").attr("data-add-sikayet-page-index");
+            console.log(attr);
+            if ((typeof attr != typeof undefined) && (attr != false))
+            {
+                console.log("attribute exists");
+                if (attr == 1)
+                {
+                    window.location.href = "sema_3_1_2_x.html";
+                }
+                else if (attr == 2)
+                {
+                    window.location.href = "sema_3_1_3_x.html";
+                }
+                else if (attr == 3)
+                {
+                    window.location.href = "sema_3_1_4_0.html";
+                }
+                else if (attr == 4)
+                {
+                    window.location.href = "sema_3_1_5_0.html";
+                }
+                else if (attr == 5)
+                {
+                    //  TO DO: preview sayfasina yolla
+                    window.location.href = "sema_3.html";
+                }
+            }
+        }
+    })
 })
