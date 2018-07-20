c8e20a0728f2dc048d8f912a0be58f6c69910759
unlut <turkerunlu@hotmail.com>
2018-07-20 13:25:24 +0300

    fm query screen

---
+++ fm_navigation.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    //  anasayfa button
+    $("#adminButton0").on("click", function() {
+        window.location.href = "admin_2.html"
+    });
+
+    //  sikayetler button
+    $("#adminButton1").on("click", function() {
+        window.location.href = "admin_3.html";
+    });
+
+    //  bekleyenleri goster button
+    $("#adminButton2").on("click", function() {
+        window.location.href = "admin_4.html";
+    });
+
+    //  filtre ekle button
+    $("#adminButton3").on("click", function() {
+        window.location.href = "admin_6.html";
+    });
+
+    //  istatistikler button
+    $("#adminButton4").on("click", function() {
+        window.location.href = "admin_19.html";
+    });
+
+    //  cikis button
+    $("#adminButton5").on("click", function() {
+        window.location.href = "admin_1.html";
+    });
+
+})
